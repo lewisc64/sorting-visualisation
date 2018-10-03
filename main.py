6999aab68148a72c37b8ee5b1c10eda6557325f5
redmechanic <redmechanic_@outlook.com>
2018-10-03 21:18:42 +0100

    added basic GUI

--- main.py
+++ main.py
@@ -13,8 +13,64 @@ FPS = 60
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+class Button:
+    def __init__(self, text, value, x, y, width, height):
+        self.text = text
+        self.value = value
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def handle(self, e):
+        if e.type == pygame.MOUSEBUTTONUP:
+            x, y = e.pos
+            if x >= self.x and x < self.x + self.width:
+                if y >= self.y and y < self.y + self.height:
+                    return True
+        
+        return False
+
+    def get_rect(self):
+        return (self.x, self.y, self.width, self.height)
+    
+    def draw(self, surface):
+        font = pygame.font.SysFont("Consolas", self.height // 2)
+        pygame.draw.rect(surface, (255, 255, 255), self.get_rect())
+        rendered = font.render(self.text, 1, (0, 0, 0))
+        surface.blit(rendered, ((self.x + self.width // 2) - rendered.get_width() // 2,
+                                (self.y + self.height // 2) - rendered.get_height() // 2))
+
 def menu():
-    pass
+    buttons = []
+    button_height = 30
+    button_width = 200
+    padding = 10
+    x = padding
+    y = padding
+    for sort in sorts:
+        buttons.append(Button(sort, sort, x, y, button_width, button_height))
+        y += button_height + padding
+    
+    while True:
+
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+            for button in buttons:
+                if button.handle(e):
+                    sorting(button.value)
+
+        display.fill((0, 0, 0))
+        
+        for button in buttons:
+            button.draw(display)
+
+        pygame.display.update()
+        clock.tick(FPS)
+        
 
 def sorting(method):
     n = 200
@@ -24,18 +80,22 @@ def sorting(method):
     data.delay = 0.01
     thread = perform(method, data)
 
-    while True:
+    while thread.isAlive():
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+                
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_ESCAPE:
+                    data.delay = 0
         
         data.draw(display, 0, 0, WIDTH, HEIGHT)
 
         pygame.display.update()
 
 if __name__ == "__main__":
-    sorting("merge_sort")
+    menu()
 
 
 
